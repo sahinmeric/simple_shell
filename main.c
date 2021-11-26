99604200b1cd7a4e3490e2f98db9964be2ea9cc0
Sahin <sahin.meric86@gmail.com>
2021-11-26 08:03:13 -0800

    main.c created

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+* main - call the shell by Arturo and Juani
+* @ac: argument count
+* @av: argument vector
+* @en: external variable environment parsed by lines
+* Return: nothing
+*/
+int main(void)
+{
+	my_shell();
+	return (0);
+}
