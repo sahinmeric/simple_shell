b41d819b49e9110243765eab9e8b2e18137ce6bc
Sahin <sahin.meric86@gmail.com>
2021-11-26 08:45:20 -0800

    reading line

---
+++ simple_sh.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* my_shell - execute a simple shell
+* Return: nothing
+*/
+
+int my_shell(void)
+{
+	char *buffer;
+	size_t bufsize = 32;
+	size_t characters;
+
+	buffer = malloc(bufsize * sizeof(char));
+	printf("$");
+	characters = getline(&buffer, &bufsize, stdin);
+
+	printf("%lu characters were read.\n", characters);
+	printf("you typed: %s\n", buffer);
+	return (0);
+}
