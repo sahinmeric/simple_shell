657624798f5618563e94243840fb96478a2188dc
lulimsky <3337@holbertonschool.com>
2021-12-03 15:12:54 -0800

    on execve

--- execute.c
+++ execute.c
@@ -1,5 +1,12 @@
 #include "main.h"
 /*TODO betty*/
+/**
+ * execute - process id and path execution
+ * @argv: pointer to the filenames
+ * @token: string of path
+ * @env : pointer to shell variables
+ * Return: always 1 to continue the loop
+ */
 
 
 int execute(char *token, char *argv[], char *env[])
@@ -8,14 +15,17 @@ int execute(char *token, char *argv[], char *env[])
 	int status;
 
 	child_pid = fork();
+	/* i'm the parent process */
 	if (child_pid == -1)
 	{
-		perror("Error:");
+		perror("Error: failed process\n");
 		return (1);
-	}
+	} /* i'm the child process */
 	if (child_pid == 0)
 	{
-		execve(token, argv, env); /*TODO handle error, when it returns -1 it means path not found and will return errno,print errno to screen in case of not existence of the path*/
+		execve(token, argv, env);
+		printf("Success\n");
+		/*TODO handle error, when it returns -1 it means path not found and will return errno,print errno to screen in case of not existence of the path*/
 	}
 	else
 	{
