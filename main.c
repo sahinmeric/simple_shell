fb560cfbb27ad32411ef45e12888796899fae011
lulimsky <3337@holbertonschool.com>
2021-12-07 11:55:10 -0800

    main.c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 *@env: environment variables
 * Return: 0 on success, non-zero value on fail.
 */
+
 int main(int argc, char *argv[], char *env[])
 {
 	if (argc == 1) /*Our program doesn't accept more than 1 argument*/
